a51e2150bdd2632f9182d1e4ce3bdfa26e51681c
a.razumova <a.razumova@fun-box.ru>
2018-05-04 10:05:53 +0400

    Create gem.

---
+++ lib/activestorage/webdav.rb
@@ -0,0 +1,7 @@
+require "activestorage/webdav/version"
+
+module Activestorage
+  module Webdav
+    # Your code goes here...
+  end
+end
---
+++ lib/activestorage/webdav/version.rb
@@ -0,0 +1,5 @@
+module Activestorage
+  module Webdav
+    VERSION = "0.1.0"
+  end
+end
