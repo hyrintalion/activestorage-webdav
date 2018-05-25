4bff020de09848e749cea38dd0b24fc95e8ef316
a.razumova <a.razumova@fun-box.ru>
2018-05-25 17:09:43 +0400

    Delete an unnecessary test.

--- spec/activestorage-webdav_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe ActiveStorageWebdav do
-  it 'has a version number' do
-    expect(ActivestorageWebdav::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(false)
-  end
-end
